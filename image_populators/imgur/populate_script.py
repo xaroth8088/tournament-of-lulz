088944789b8a52426874a6206c18e341f2ef7f2f
Geoffrey Benson <xaroth@bebs.net>
2014-09-08 09:37:54 -0700

    PEP8 formatting

--- image_populators/imgur/populate_script.py
+++ image_populators/imgur/populate_script.py
@@ -1,16 +1,20 @@
 """ The script to actually run the populator
 """
 
-from imgur_populator import ImgurPopulator
 import configparser
-import os, sys
+import os
+import sys
+
+from imgur_populator import ImgurPopulator
+
 
 def main():
-	config = configparser.ConfigParser()
-	config.read("%s/imgur.conf" % (os.path.dirname(os.path.abspath(sys.argv[0])),))
+    config = configparser.ConfigParser()
+    config.read("%s/imgur.conf" % (os.path.dirname(os.path.abspath(sys.argv[0])),))
 
-	populator = ImgurPopulator(config)
-	populator.populate()
+    populator = ImgurPopulator(config)
+    populator.populate()
+
 
 if __name__ == "__main__":
-	main()
+    main()
