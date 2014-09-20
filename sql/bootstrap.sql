5724b68867e259157613798142a568247e54693f
Geoffrey Benson <xaroth@bebs.net>
2014-09-20 12:59:16 -0700

    Update DB creation script.

--- sql/bootstrap.sql
+++ sql/bootstrap.sql
@@ -6,12 +6,20 @@ USE tournament_of_lulz;
 
 CREATE TABLE `images` (
   `image_id` int(11) NOT NULL AUTO_INCREMENT,
-  `page_url` varchar(512) DEFAULT NULL,
+  `image_url_hash` varchar(32) DEFAULT NULL,
   `image_url` varchar(512) DEFAULT NULL,
+  `page_url` varchar(512) DEFAULT NULL,
   `thumbnail_url` varchar(512) DEFAULT NULL,
   `title` varchar(512) DEFAULT NULL,
   `rating` double DEFAULT '1500',
   `rd` double DEFAULT '350',
   `volatility` double DEFAULT '0.06',
-  PRIMARY KEY (`image_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`image_id`),
+  UNIQUE KEY `image_url_hash_UNIQUE` (`image_url_hash`)
+) ENGINE=InnoDB AUTO_INCREMENT=1028 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `tournaments` (
+  `tournament_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `start_time` datetime NOT NULL,
+  PRIMARY KEY (`tournament_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8;
