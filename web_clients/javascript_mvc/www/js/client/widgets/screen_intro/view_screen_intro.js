5e0fdfe3a514020656ad4a598e79a77617f78199
xaroth <xaroth@gmail.com>
2014-09-21 15:43:33 -0700

    A more exciting victory screen.

--- web_clients/javascript_mvc/www/js/client/widgets/screen_intro/view_screen_intro.js
+++ web_clients/javascript_mvc/www/js/client/widgets/screen_intro/view_screen_intro.js
@@ -1,44 +1,44 @@
 /***
-	Intro Screen View
-	Shows the top 10 images, and presents "start tournament" functionality
-***/
+ Intro Screen View
+ Shows the top 10 images, and presents "start tournament" functionality
+ ***/
 define(['require', 'jsclass/min/core', 'client/base/view', 'client/widgets/image/widget_image'], function (require) {
-	'use strict';
-	var View = require('client/base/view'),
-		WidgetImage = require('client/widgets/image/widget_image');
+    'use strict';
+    var View = require('client/base/view'),
+        WidgetImage = require('client/widgets/image/widget_image');
 
-	return new JS.Class(View, {
-		'initialize': function() {
-			this.callSuper(['top_images_model']);
-		},
+    return new JS.Class(View, {
+        'initialize': function () {
+            this.callSuper(['top_images_model']);
+        },
 
-		'_initTemplate': function() {
-			this.callSuper();
-			this.container.addClass('widget_screen_intro');
+        '_initTemplate': function () {
+            this.callSuper();
+            this.container.addClass('widget_screen_intro');
 
-			this.container.append('\
+            this.container.append('\
 				<button>Start Tournament</button>\
 			');
 
-			this.container.find('button').click( $.proxy( this._controller.onStartPressed, this._controller ) );
-		},
+            this.container.find('button').click($.proxy(this._controller.onStartPressed, this._controller));
+        },
 
-		'_draw': function() {
-			var i, len, image_widget;
+        '_draw': function () {
+            var i, len, image_widget;
 
-			this.removeAllSubwidgets();
+            this.removeAllSubwidgets();
 
-			// Add image widgets for each one in the top_images_model
-			for( i = 0, len = this.models.top_images_model.images.length; i < len; i++ ) {
-				image_widget = new WidgetImage.controller( this._controller, new WidgetImage.view(), {
-					'image_model': this.models.top_images_model.images[i] 
-				});
-				this.addSubwidget( image_widget, this.container );
-				image_widget.view.container.delay( 500 * i ).animate( {
-						'top': '100%'
-					}, 3000
-				);
-			}
-		}
-	});
+            // Add image widgets for each one in the top_images_model
+            for (i = 0, len = this.models.top_images_model.images.length; i < len; i++) {
+                image_widget = new WidgetImage.controller(this._controller, new WidgetImage.view(), {
+                    'image_model': this.models.top_images_model.images[i]
+                });
+                this.addSubwidget(image_widget, this.container);
+                image_widget.view.container.delay(500 * i).animate({
+                        'top': '100%'
+                    }, 3000
+                );
+            }
+        }
+    });
 });
--- web_clients/javascript_mvc/www/js/client/widgets/victory/view_victory.js
+++ web_clients/javascript_mvc/www/js/client/widgets/victory/view_victory.js
@@ -2,21 +2,34 @@
 	Victory View
 	A brief celebration of the ultimate tournament winner
 ***/
-define(['require', 'jsclass/min/core', 'client/base/view'], function (require) {
+define(['require', 'jsclass/min/core', 'client/base/view', 'confetti/confetti', 'jquery.transit/jquery.transit'], function (require) {
 	'use strict';
 	var View = require('client/base/view');
 	
 	return new JS.Class(View, {
 		'initialize': function() {
 			this.callSuper(['tournament_model']);
+            this.timer = null;
 		},
 
 		'_initTemplate': function() {
+            var victory;
 			this.callSuper();
 
 			this.container.addClass( 'widget_victory' );
-			this.container.append("<div class='victory_text'>THE WINNER!</div><img class='winner' /><button class='goback'>Play Again!</button>");
+			this.container.append("<img class='winner' /><button class='goback'>Play Again!</button>");
 			this.container.find('.goback').click($.proxy(this._controller.onPlayAgainPressed, this._controller));
+
+            // Victory!
+            victory = $("<div/>").addClass('victory_image');
+            this.container.append(victory);
+            victory.transition({
+                scale: 20
+            }).transition({
+                scale: 1
+            }, 1000, 'easeInQuad').transition({
+                opacity: 0
+            }, 3000, 'easeInQuad');
 		},
 
 		'_draw': function() {
@@ -24,6 +37,20 @@ define(['require', 'jsclass/min/core', 'client/base/view'], function (require) {
 
 			winner = this.models.tournament_model.getWinnerData();
 			this.container.find('.winner').attr('src', winner.image_url);
-		}
+
+            // Confetti!
+            clearInterval(this.timer);
+
+            this.timer = setInterval(function() {
+                CONFETTIS_PER_SHAKE = 50;
+
+                startConfetti();
+            }, 1000);
+		},
+
+        'destroy': function() {
+            clearInterval(this.timer);
+            this.callSuper();
+        }
 	});
 });
