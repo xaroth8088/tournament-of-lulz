d50e03d7c45795b2810c0052beb983cc4504c1cf
xaroth <xaroth@gmail.com>
2014-09-21 17:59:23 -0700

    Title screen is pretty now.

--- web_clients/javascript_mvc/www/js/client/main.js
+++ web_clients/javascript_mvc/www/js/client/main.js
@@ -6,13 +6,26 @@ define(['require', 'jquery', './widgets/page/widget_page'], function (require) {
 	var WidgetPage = require('./widgets/page/widget_page');
 	
 	function main() {
-		var page_widget;
-		
-		page_widget = new WidgetPage.controller( null, new WidgetPage.view(), {
-			'page_model': new WidgetPage.model() 
-		});
-		page_widget.start();
-		$(document.body).append(page_widget.view.container);
+		var page_widget, loading_spinner, loading_text;
+
+        // Remove the loading spinner
+        loading_spinner = $('.spinner');
+        loading_text = $('.initial_loading_text');
+
+        loading_spinner.animate({
+            opacity: 0
+        }, 500);
+        loading_text.animate({
+            opacity: 0
+        }, 500, function(){
+            loading_spinner.remove();
+            loading_text.remove();
+            page_widget = new WidgetPage.controller( null, new WidgetPage.view(), {
+                'page_model': new WidgetPage.model()
+            });
+            page_widget.start();
+            $(document.body).append(page_widget.view.container);
+        });
 	}
 
 	$(document).ready( main );
--- web_clients/javascript_mvc/www/js/client/widgets/screen_intro/view_screen_intro.js
+++ web_clients/javascript_mvc/www/js/client/widgets/screen_intro/view_screen_intro.js
@@ -2,7 +2,7 @@
  Intro Screen View
  Shows the top 10 images, and presents "start tournament" functionality
  ***/
-define(['require', 'jsclass/min/core', 'client/base/view', 'client/widgets/image/widget_image'], function (require) {
+define(['require', 'jsclass/min/core', 'client/base/view', 'client/widgets/image/widget_image', 'jquery.transit/jquery.transit'], function (require) {
     'use strict';
     var View = require('client/base/view'),
         WidgetImage = require('client/widgets/image/widget_image');
@@ -10,35 +10,47 @@ define(['require', 'jsclass/min/core', 'client/base/view', 'client/widgets/image
     return new JS.Class(View, {
         'initialize': function () {
             this.callSuper(['top_images_model']);
+            this.timer = null
+            this.image_shown = 0;
         },
 
         '_initTemplate': function () {
             this.callSuper();
             this.container.addClass('widget_screen_intro');
 
-            this.container.append('\
-				<button>Start Tournament</button>\
-			');
+            this.container.append('<div class="background"></div>');
+            this.container.append('<div class="title_text">Tournament Of Lulz</div>');
+            this.container.append('<button class="start_tournament">Start Tournament</button>');
 
             this.container.find('button').click($.proxy(this._controller.onStartPressed, this._controller));
         },
 
         '_draw': function () {
-            var i, len, image_widget;
-
             this.removeAllSubwidgets();
 
-            // Add image widgets for each one in the top_images_model
-            for (i = 0, len = this.models.top_images_model.images.length; i < len; i++) {
-                image_widget = new WidgetImage.controller(this._controller, new WidgetImage.view(), {
-                    'image_model': this.models.top_images_model.images[i]
-                });
-                this.addSubwidget(image_widget, this.container);
-                image_widget.view.container.delay(500 * i).animate({
-                        'top': '100%'
-                    }, 3000
-                );
+            this._nextTopImage();
+            clearTimeout(this.timer);
+            this.timer = setInterval($.proxy(this._nextTopImage, this), 5000);
+        },
+
+        '_nextTopImage': function() {
+            var len;
+
+            len = this.models.top_images_model.images.length;
+            if( len === 0 ) {
+                return;
             }
+
+            this.container.find('.background').css({
+                'background-image': "url(" + this.models.top_images_model.images[this.image_shown % len].image_url + ")"
+            });
+
+            this.image_shown++;
+        },
+
+        'destroy': function() {
+            clearInterval(this.timer);
+            this.callSuper();
         }
     });
 });
