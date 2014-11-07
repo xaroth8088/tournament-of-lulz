3f7d17e65cbacb20b8d97ea86bf4d5f7cf081f8b
Geoffrey Benson <xaroth@bebs.net>
2014-11-07 00:45:24 -0800

    Layout changes for selecting screen to show title and score.

--- web_clients/javascript_mvc/www/js/client/widgets/selecting/view_selecting.js
+++ web_clients/javascript_mvc/www/js/client/widgets/selecting/view_selecting.js
@@ -22,10 +22,18 @@ define([
 
 			this.container.addClass( 'widget_selecting' );
 			this.container.html("\
-    			<div class='img_holder image_1'>\
-			    </div>\
-			    <div class='versus'></div>\
-			    <div class='img_holder image_2'>\
+			    <div class='images'>\
+                    <div class='img_holder'>\
+                        <div class='image image_1'></div>\
+                        <div class='title title_1'></div>\
+                    </div>\
+                    <div class='score_tag player_1'></div>\
+                    <div class='versus'></div>\
+                    <div class='score_tag player_2'></div>\
+                    <div class='img_holder'>\
+                        <div class='image image_2'></div>\
+                        <div class='title title_2'></div>\
+                    </div>\
 			    </div>\
             ");
 
@@ -82,7 +90,7 @@ define([
 		},
 
 		'_draw': function() {
-			var image_1, image_2;
+			var image_1, image_2, player_1_score, player_2_score;
 
 			image_1 = this.container.find('.image_1');
 			image_2 = this.container.find('.image_2');
@@ -95,6 +103,14 @@ define([
             image_2.css({
                 'background-image': 'url(' + this.match.player_2.image_url + ')'
             });
+
+            this.container.find('.title_1').text(this.match.player_1.title);
+            this.container.find('.title_2').text(this.match.player_2.title);
+
+            player_1_score = (Math.floor(this.match.player_1.rating) + '').split('').join('<br/>');
+            player_2_score = (Math.floor(this.match.player_2.rating) + '').split('').join('<br/>');
+            this.container.find('.player_1').html(player_1_score);
+            this.container.find('.player_2').html(player_2_score);
 		},
 
 		'_onImage1Clicked': function() {
